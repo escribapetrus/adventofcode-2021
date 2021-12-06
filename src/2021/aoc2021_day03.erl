617dc9bb88006d2f3b84afd81e0d940e2fa5ca25
Pedro Schreiber <schreiber.mmb@gmail.com>
2021-12-06 18:14:22 -0300

    solve day 3 pt 2

--- src/2021/aoc2021_day03.erl
+++ src/2021/aoc2021_day03.erl
@@ -1,50 +1,103 @@
 -module(aoc2021_day03).
 -behaviour(aoc_puzzle).
--export([parse/1, solve01/1, solve02/1, reduce/3, reduce/2, reduce/1, gamma/1, epsilon/1, parse_bits/1]).
--import(lists, [map/2, foldl/3, reverse/1, duplicate/2]).
+-export([
+    parse_count/1, 
+    solve01/1, 
+    solve02/1, 
+    reduce/3, 
+    reduce/2, 
+    reduce/1,
+    gamma/1, 
+    epsilon/1, 
+    convert/1, 
+    filter_by_index/3,
+    parse/1]).
+-import(lists, [
+    map/2, 
+    foldl/3, 
+    reverse/1, 
+    duplicate/2, 
+    filter/2, 
+    nth/2]).
 -import(string, [split/2, trim/1, split/2]).
 
-parse(Inputs) ->
+-type raw_input() :: list(string()).
+-type count() :: {integer(), integer()}.
+-type bin_list() :: list(integer()).
+
+solve01(Inputs) -> 
+    Parsed = parse(Inputs),
+    Counted = parse_count(Parsed),
+    Reduced = reduce(Counted),
+    {Gamma, Epsilon} = {gamma(Reduced), epsilon(Reduced)},
+    convert(Gamma) * convert(Epsilon).
+
+solve02(Inputs) -> 
+    Parsed = parse(Inputs),
+    {Oxygen, CO2} = {oxygen(Parsed), co2(Parsed)},
+    erlang:display({Oxygen, CO2}),
+    convert(Oxygen) * convert(CO2).
+
+-spec parse(raw_input()) -> list(bin_list()).
+parse(Inputs) -> 
     Format = fun(X) -> 
         Trimmed = trim(X),
-        Mapped = map(fun parse_int/1, Trimmed),
-        map(fun count/1, Mapped)
+        map(fun parse_int/1, Trimmed)
     end,
     map(Format, Inputs).
 
-parse_bits(List) ->
+-spec parse_count(raw_input()) -> list(count()).
+parse_count(Inputs) ->
+    Format = fun(X) -> 
+        map(fun count/1, X)
+    end,
+    map(Format, Inputs).
+
+-spec convert(bin_list()) -> integer().
+convert(List) ->
     Size = length(List),
     Bin = construct_bin(List),
     <<Value:(Size)>> = Bin,
     Value.
 
+-spec construct_bin(bin_list()) -> bitstring().
 construct_bin(List) ->
     construct_bin(List, <<>>).
-
 construct_bin([], Acc) -> Acc;
 construct_bin([X | T], Acc) -> 
     construct_bin(T, <<Acc/bitstring, X:1>>).
 
-solve01(Inputs) -> 
-    Reduced = reduce(Inputs),
-    Gamma = gamma(Reduced),
-    Epsilon = epsilon(Reduced),
-    parse_bits(Gamma) * parse_bits(Epsilon).
-
-solve02(_Inputs) -> 2.
-
+-spec count(integer()) -> count().
 count(Bin) -> 
     case Bin of
         0 -> {1, 0};
         1 -> {0, 1}
     end.
 
+-spec parse_int(integer()) -> integer().
 parse_int(I) ->
     case I of
         48 -> 0;
         49 -> 1
     end.
 
+gamma({Zeros, Ones}) -> 
+    if 
+        Ones == Zeros -> 1;
+        Ones > Zeros -> 1;
+        true -> 0 
+    end;
+gamma(Pairs) -> map(fun gamma/1, Pairs).
+
+epsilon({Zeros, Ones}) -> 
+    if
+        Ones == Zeros -> 0;
+        Ones > Zeros -> 0;
+        true -> 1
+    end;
+epsilon(Pairs) -> map(fun epsilon/1, Pairs).
+
+-spec reduce(list(count())) -> count().
 reduce(List) -> 
     [A|_] = List,
     Zeros = duplicate(length(A), {0,0}),
@@ -59,17 +112,28 @@ reduce([], [], Res) -> reverse(Res);
 reduce([{A, B}|T], [{C, D}|S], Res) -> 
     reduce(T, S, [{A + C, B + D}| Res]).
 
-gamma({Zeros, Ones}) -> 
-    if 
-        Zeros >= Ones -> 0;
-        true -> 1 
-    end;
-gamma(Pairs) -> map(fun gamma/1, Pairs).
+-spec filter_by_index(bin_list(), integer(), integer()) -> bin_list().
+filter_by_index(List, El, Index) -> 
+    F = fun(X) -> nth(Index, X) == El end,
+    filter(F, List).
 
-epsilon({Zeros, Ones}) -> 
-    if
-        Ones >= Zeros -> 0;
-        true -> 1
-    end;
-epsilon(Pairs) -> map(fun epsilon/1, Pairs).
+oxygen(Inputs) -> oxygen(Inputs, 1).
+oxygen([H|[]], _) -> H;
+oxygen(Inputs, I) ->
+    Counted = parse_count(Inputs),
+    Reduced = reduce(Counted),
+    Pattern = gamma(Reduced),
+    Filtered = filter_by_index(Inputs, nth(I, Pattern), I),
+    oxygen(Filtered, I + 1).
+
+co2(Inputs) -> co2(Inputs, 1).
+co2([H|[]], _) -> H;
+co2(Inputs, I) ->
+    Counted = parse_count(Inputs),
+    Reduced = reduce(Counted),
+    Pattern = epsilon(Reduced),
+    Filtered = filter_by_index(Inputs, nth(I, Pattern), I),
+    co2(Filtered, I + 1).
+
+
 
--- test/aoc2021_day03_test.erl
+++ test/aoc2021_day03_test.erl
@@ -3,61 +3,98 @@
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("stdlib/include/assert.hrl").
 
--import(aoc2021_day03, [parse/1, solve01/1, solve02/1, reduce/3, reduce/1, gamma/1, epsilon/1, parse_bits/1]).
+-import(aoc2021_day03, [
+    parse/1, 
+    parse_count/1, 
+    solve01/1, 
+    solve02/1, 
+    reduce/3, 
+    reduce/1, 
+    gamma/1, 
+    epsilon/1, 
+    convert/1, 
+    filter_by_index/3,
+    filter_inputs/2]).
 -import(inputs, [readlines/1]).
 
-solution_01_test() -> 
-    Inputs = parse(readlines("priv/inputs/2021/input03")),
+solve01_test() -> 
+    Inputs = [
+        "00100",
+        "11110",
+        "10110",
+        "10111",
+        "10101",
+        "01111",
+        "00111",
+        "11100",
+        "10000",
+        "11001",
+        "00010",
+        "01010"
+    ],
     Res = solve01(Inputs),
 
-    ?assertEqual(0, Res).
+    ?assertEqual(198, Res).
+
+solve02_test() ->
+    Inputs = [
+        "00100",
+        "11110",
+        "10110",
+        "10111",
+        "10101",
+        "01111",
+        "00111",
+        "11100",
+        "10000",
+        "11001",
+        "00010",
+        "01010"
+    ],    
+    Res = solve02(Inputs),
+
+    ?assertEqual(230, Res).
 
 parse_test() ->
     Inputs = readlines("priv/inputs/2021/input03"),
     [H|_T] = parse(Inputs),
-    Expected = [
-        {0,1},
-        {1,0},
-        {1,0},
-        {1,0},
-        {1,0},
-        {1,0},
-        {0,1},
-        {1,0},
-        {0,1},
-        {0,1},
-        {1,0},
-        {0,1}
-    ],
+    Expected = [1,0,0,0,0,0,1,0,1,1,0,1],
 
     ?assertEqual(Expected, H).
 
-parse_bits_test() -> 
+parse_count_test() ->
+    Inputs = readlines("priv/inputs/2021/input03"),
+    [H|_T] = parse_count(parse(Inputs)),
+    Expected = [{0,1},{1,0},{1,0},{1,0},{1,0},{1,0},{0,1},{1,0},{0,1},{0,1},{1,0},{0,1}],
+
+    ?assertEqual(Expected, H).
+
+convert_test() -> 
     Bits1 = [1,1],
     Bits2 = [1,0,1,1,0],
-    {A, B} = {parse_bits(Bits1), parse_bits(Bits2)},
+    {A, B} = {convert(Bits1), convert(Bits2)},
 
     ?assertEqual(3, A),
     ?assertEqual(22, B).
 
 gamma_test() ->
-        Inputs = parse([
+    Inputs = parse_count(parse([
         "00100",
         "11110",
         "10110",
         "10111"
-    ]),
+    ])),
     Reduced = reduce(Inputs),
     Gamma = gamma(Reduced),
     ?assertEqual([1,0,1,1,0], Gamma).
 
 epsilon_test() ->
-        Inputs = parse([
+    Inputs = parse_count(parse([
         "00100",
         "11110",
         "10110",
         "10111"
-    ]),
+    ])),
     Reduced = reduce(Inputs),
     Epsilon = epsilon(Reduced),
     
@@ -84,11 +121,27 @@ reduce_3_test() ->
     ?assertEqual(Expected,Res).
 
 reduce_1_test() -> 
-    Inputs = parse([
+    Inputs = parse_count(parse([
         "00100",
         "11110",
         "10110",
         "10111"
-    ]),
+    ])),
     Res = reduce(Inputs),
     ?assertEqual([{1,3},{3,1},{0,4},{1,3},{3,1}], Res).
+
+filter_by_index_test() ->
+    Inputs = [
+        [0,0,1,0,0],
+        [1,1,1,1,0],
+        [1,0,1,1,0],
+        [1,0,1,1,1]
+    ],
+    Expected = [
+        [1,1,1,1,0],
+        [1,0,1,1,0],
+        [1,0,1,1,1]
+    ],
+    Res = filter_by_index(Inputs, 1, 4),
+
+    ?assertEqual(Expected,Res). 
